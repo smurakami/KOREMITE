0cc665872b16c1ed32f1dd4262dab6a99a39c9d5
s.murakami <smurakami.84@gmail.com>
2018-01-29 18:12:33 +0900

    Initial Commit

---
+++ UeShita/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UeShita
+//
+//  Created by 村上晋太郎 on 2018/01/29.
+//  Copyright © 2018年 村上晋太郎. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ UeShita/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UeShita
+//
+//  Created by 村上晋太郎 on 2018/01/29.
+//  Copyright © 2018年 村上晋太郎. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
